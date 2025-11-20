fbd8b00e03c63f816711073599b48ce29fb99b12
ShunNebula <vyach1369@gmail.com>
2025-11-20 09:42:37 +0700

    Создание начального меню

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void settings()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void backmenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void quite()
+    {
+        Application.Quit();
+    }
+}

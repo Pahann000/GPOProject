230da150beef477925a2546359b0f539bea8f82b
ShunNebula <vyach1369@gmail.com>
2026-02-20 17:09:08 +0700

    # Создана "шина событий", через которую будет происходить общение систем.

---
+++ Assets/scripts/Core/EventBus.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collection.Generic;
+using UnityEngine;
+
+public class EventBus
+{
+	private readonly Dictionary<Type, List<object>> _subscribers = new Dictionary<Type, List<object>>();
+
+	/// <summary>
+	/// Подписаться на событие.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="callback"></param>
+	public void Subscribe<T>(Action<T> callback) where T : IGameEvent
+	{
+		Type type = typeof(t);
+
+		if (!_subscribers.ContainsKey(type))
+		{
+			_subscribers[type] = new List<object>();
+		}
+
+		_subscribers[type].Add(callback);
+	}
+
+	/// <summary>
+	/// Отписаться от события.
+	/// Не забыть вызвать при уничтожении объекта!
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="callback"></param>
+	public void Unsubscribe<T>(Action<T> callback) where T : IGameEvent
+	{
+		Type type = typeof(T);
+
+		if (_subscribers.ContainsKey(type))
+		{
+			_subscribers[type].Remove(callback);
+		}
+	}
+
+	public void Raise<T>(T eventData) where T : IGameEvent
+	{
+		Type type = typeof(T);
+
+		if (_subscribers.TryGetValue(type, out List<object> callbacks))
+		{
+			foreach (var obj in callbacks)
+			{
+				var callback = obj as Action<T>;
+				try
+				{
+					callback?.Invoke(eventData);
+				}
+				catch (Expection e)
+				{
+					Debug.LogError($"[EventBus] Ошибка при обработке события {type.Name}: {e}")
+				}
+			}
+		}
+	}
+}
